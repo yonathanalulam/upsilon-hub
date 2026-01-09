be13dec92c97d92e54eac9a8af90bdfd1faae078
YonathanAlula <alulayoni19@gmail.com>
2026-01-09 07:12:25 +0300

    Add SEO tags and Sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,9 @@
-// astro.config.mjs
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [
-    tailwind(), 
-    react()
-  ],
+ 
+  site: 'https://upsilon-hub.vercel.app', 
+  integrations: [tailwind(), sitemap()],
 });
