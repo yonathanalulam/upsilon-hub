92021774987be48c9f28723962a453a3e6c89ae8
YonathanAlula <alulayoni19@gmail.com>
2026-01-09 07:22:47 +0300

    Fix: Add React integration to config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
+import react from '@astrojs/react'; 
 
 export default defineConfig({
+  
   site: 'https://upsilon-hub.vercel.app', 
-  integrations: [tailwind(), sitemap()],
+  integrations: [
+    tailwind(), 
+    sitemap(), 
+    react() 
+  ],
 });
