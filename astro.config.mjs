b248a78d0a0579863039645dda758ee324b6f395
YonathanAlula <alulayoni19@gmail.com>
2026-01-09 07:19:11 +0300

    Add Google Verification Tag

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
- 
   site: 'https://upsilon-hub.vercel.app', 
   integrations: [tailwind(), sitemap()],
 });
